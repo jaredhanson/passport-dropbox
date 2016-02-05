b2992deee27fa63d469779c3c256fb2f5879ff55
Jared Hanson <jaredhanson@gmail.com>
2016-02-05 10:44:18 -0800

    Improve error handling.

--- test/strategy.profile.test.js
+++ test/strategy.profile.test.js
@@ -57,6 +57,70 @@ describe('Strategy#userProfile', function() {
     });
   }); // fetched from default endpoint
   
+  describe('error caused by invalid token', function() {
+    var strategy = new DropboxStrategy({
+      consumerKey: 'ABC123',
+      consumerSecret: 'secret'
+    }, function verify(){});
+    
+    strategy._oauth.get = function(url, token, tokenSecret, callback) {
+      var body = '{"error": "Access token not found."}';
+      callback({ statusCode: 401, data: body });
+    }
+    
+    
+    var err, profile;
+    
+    before(function(done) {
+      strategy.userProfile('x-token', 'token-secret', { user_id: '123' }, function(e, p) {
+        err = e;
+        profile = p;
+        done();
+      });
+    });
+  
+    it('should error', function() {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.message).to.equal("Access token not found.");
+    });
+    
+    it('should not supply profile', function() {
+      expect(profile).to.be.undefined;
+    });
+  }); // error caused by invalid token
+  
+  describe('error caused by invalid token secret', function() {
+    var strategy = new DropboxStrategy({
+      consumerKey: 'ABC123',
+      consumerSecret: 'secret'
+    }, function verify(){});
+    
+    strategy._oauth.get = function(url, token, tokenSecret, callback) {
+      var body = '{"error": "Unauthorized"}';
+      callback({ statusCode: 401, data: body });
+    }
+    
+    
+    var err, profile;
+    
+    before(function(done) {
+      strategy.userProfile('x-token', 'token-secret', { user_id: '123' }, function(e, p) {
+        err = e;
+        profile = p;
+        done();
+      });
+    });
+  
+    it('should error', function() {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.message).to.equal("Unauthorized");
+    });
+    
+    it('should not supply profile', function() {
+      expect(profile).to.be.undefined;
+    });
+  }); // error caused by invalid token secret
+  
   describe('error caused by malformed response', function() {
     var strategy = new DropboxStrategy({
       consumerKey: 'ABC123',
--- test/strategy.test.js
+++ test/strategy.test.js
@@ -23,4 +23,66 @@ describe('Strategy', function() {
     });
   })
   
+  describe('error caused by invalid consumer key sent to request token URL', function() {
+    var strategy = new DropboxStrategy({
+      consumerKey: 'ABC123',
+      consumerSecret: 'invalid-secret'
+    }, function verify(){});
+    
+    strategy._oauth.getOAuthRequestToken = function(params, callback) {
+      callback({ statusCode: 401, data: '{"error": "Invalid app key (consumer key). Check your app\'s configuration to make sure everything is correct."}' });
+    }
+    
+    
+    var err;
+  
+    before(function(done) {
+      chai.passport.use(strategy)
+        .error(function(e) {
+          err = e;
+          done();
+        })
+        .req(function(req) {
+          req.session = {};
+        })
+        .authenticate();
+    });
+  
+    it('should error', function() {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.message).to.equal("Invalid app key (consumer key). Check your app's configuration to make sure everything is correct.");
+    });
+  });
+  
+  describe('error caused by invalid consumer secret sent to request token URL', function() {
+    var strategy = new DropboxStrategy({
+      consumerKey: 'ABC123',
+      consumerSecret: 'invalid-secret'
+    }, function verify(){});
+    
+    strategy._oauth.getOAuthRequestToken = function(params, callback) {
+      callback({ statusCode: 401, data: '{"error": "Unauthorized"}' });
+    }
+    
+    
+    var err;
+  
+    before(function(done) {
+      chai.passport.use(strategy)
+        .error(function(e) {
+          err = e;
+          done();
+        })
+        .req(function(req) {
+          req.session = {};
+        })
+        .authenticate();
+    });
+  
+    it('should error', function() {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.message).to.equal("Unauthorized");
+    });
+  });
+  
 });
